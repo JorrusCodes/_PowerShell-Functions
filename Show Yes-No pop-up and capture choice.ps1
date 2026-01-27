6ea6d9d4804760a3914123661bc97dadd64ff43c
JorrusCodes <johnphilipsheats@gmail.com>
2026-01-26 21:47:08 -0500

    Update Show Yes-No pop-up and capture choice.ps1

--- Show Yes-No pop-up and capture choice.ps1
+++ Show Yes-No pop-up and capture choice.ps1
@@ -2,12 +2,12 @@
     Display a "Yes"/"No" Dialog prompt and capture the clicked button
     By: John Sheats
 
-    Description: As very simply function to present a Yes/No prompt and record whether "Yes"
+    Description: A simple function to present a Yes/No prompt and record whether "Yes"
     was selected or not. "No" and null result (pop-up with closed without a button press)
     both return $false
 #>
 
-# Display a Yes/Not prompt and return $true if "Yes" was clicked and $false if not
+# Display a Yes/No prompt and return $true if "Yes" was clicked and $false if not
 function popupYesNo {
     param (
         [string]$Message = "Do you want to continue?"
@@ -28,3 +28,4 @@ function popupYesNo {
         return $false
     }
 }
+
