493b46e0d72907e9097cd01d5c41125c54b7e8da
JorrusCodes <johnphilipsheats@gmail.com>
2026-01-26 21:29:33 -0500

    Add files via upload

---
+++ Show Ok pop-up.ps1
@@ -0,0 +1,22 @@
+﻿<#
+    Display an "OK" pop-up
+    By: John Sheats
+
+    Description: Simple pop-up function to save me from digging up this method every 
+    time I want it
+#>
+
+# Display a simple pop-up with an OK button
+function popupOK {
+    param (
+        [string]$Message = "Click OK to continue..."
+    )
+    
+    # Display the Ok pop-up
+    Add-Type -AssemblyName PresentationFramework
+    $msgBoxInput = [System.Windows.MessageBox]::Show(
+        "$($Message)",
+        "Script Pop-up",
+        "Ok"
+    )
+}
---
+++ Show Yes-No pop-up and capture choice.ps1
@@ -0,0 +1,30 @@
+﻿<#
+    Display a "Yes"/"No" Dialog prompt and capture the clicked button
+    By: John Sheats
+
+    Description: As very simply function to present a Yes/No prompt and record whether "Yes"
+    was selected or not. "No" and null result (pop-up with closed without a button press)
+    both return $false
+#>
+
+# Display a Yes/Not prompt and return $true if "Yes" was clicked and $false if not
+function popupYesNo {
+    param (
+        [string]$Message = "Do you want to continue?"
+    )
+    
+    # Display the Yes/No prompt
+    Add-Type -AssemblyName PresentationFramework
+    $msgBoxInput = [System.Windows.MessageBox]::Show(
+        "$($Message)",
+        "Script Prompt",
+        "YesNo"
+    )
+
+    # Return the prompt result
+    If ($msgBoxInput -eq "Yes") {
+        return $true
+    } Else{
+        return $false
+    }
+}
